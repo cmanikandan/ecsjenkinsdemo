81d66adc19c51069cefa8fd92502e5b5eedc404e
Mani <mani.chandra@gmail.com>
2016-11-14 16:28:01 +0530

    Update index.php

--- www/index.php
+++ www/index.php
@@ -18,7 +18,7 @@
 <body>
 	<img id="logo" src="logo.png" />
 	<h1><?php echo "Hello ".($_ENV["NAME"]?$_ENV["NAME"]:"world")."!"; ?></h1>
-	<h1>This is a real test</h1>
+	<h1>This is a real test done on 14th Nov 2016</h1>
 	<?php if($_ENV["HOSTNAME"]) {?><h3>My hostname is <?php echo $_ENV["HOSTNAME"]; ?></h3><?php } ?>
 	<?php
 	$links = [];
